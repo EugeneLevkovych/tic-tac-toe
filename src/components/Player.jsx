c57b863f4045c3aeb8b034387c382be5b68a3f8b
EugeneLevkovych <eugenelevkovych@yahoo.com>
2025-04-21 17:48:17 +0300

    add Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,34 @@
 import { useState } from "preact/hooks";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    console.dir(event);
+
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        <span>{editablePlayerName}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
